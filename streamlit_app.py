08e7e00af6089e7d1751f0245272e9ce18634677
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-21 10:27:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ st.title('🦀 Rust in Streamlit')
 process1 = subprocess.Popen(["rustc", "test.rs"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
 st.write(result1)
+
+process2 = subprocess.Popen(["bash", "rush.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+result1 = process2.communicate()
+st.write(result2)
