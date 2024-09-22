a1d73f87fa930c2536251645453b159c72e55d00
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-21 23:22:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import subprocess
+from code_editor import code_editor
 
 st.title('🦀 Rust in Streamlit')
+
+response_dict = code_editor(your_code_string)
+response_dict
 
 process1 = subprocess.Popen(["rustc", "hello.rs"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 process2 = subprocess.Popen(["./hello"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
