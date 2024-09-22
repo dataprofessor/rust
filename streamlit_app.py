1c1a6c924b374bc224d32e8e79df2e18dec52e79
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-22 00:36:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,24 +12,10 @@ st.title('🦀 Rust in Streamlit')
 with open('hello.rs') as rust_file:
     rust_code = rust_file.read()
 
-btn_settings_editor_btns = [{
-    "name": "copy",
-    "feather": "Copy",
-    "hasText": True,
-    "alwaysOn": True,
-    "commands": ["copyAll"],
-    "style": {"top": "0rem", "right": "0.4rem"}
-  },{
-    "name": "update",
-    "feather": "RefreshCw",
-    "primary": True,
-    "hasText": True,
-    "showWithIcon": True,
-    "commands": ["submit"],
-    "style": {"bottom": "0rem", "right": "0.4rem"}
-  }]
+with open('btn_settings.json') as btn_file:
+    btn_settings = btn_file.read()
 
-response_dict = code_editor(rust_code, lang="rust", buttons=btn_settings_editor_btns)
+response_dict = code_editor(rust_code, lang="rust", buttons=btn_settings)
 response_dict
 
 process1 = subprocess.Popen(["rustc", "hello.rs"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
