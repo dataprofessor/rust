2041b9005804d044733dfa813129a4d319018a43
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-22 22:24:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,13 +60,13 @@ with col[0]:
     response_dict = code_editor(rust_code, lang='rust', height=12, buttons=btn_settings, key='code_editor')
     st.session_state.current_code = response_dict['text']
 
-    if st.session_state.current_code != '':
-        code_changed()
+    code_changed()
     
 with col[1]:
-    st.subheader('Code Content')
-    st.code(st.session_state.current_code, line_numbers=True)
-    
-    st.subheader('Code Output')
-    if 'rust_output' in st.session_state:
-        st.code(st.session_state.rust_output)
+    if st.session_state.current_code != '':
+        st.subheader('Code Content')
+        st.code(st.session_state.current_code, line_numbers=True)
+        
+        st.subheader('Code Output')
+        if 'rust_output' in st.session_state:
+            st.code(st.session_state.rust_output)
