cf6271b910c547e7f94b92b3e210b8b492e84407
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-10-02 22:34:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ with col[0]:
         """)
         st.markdown("""**Overview**
         
-            - First, we'll create a file called *hello.rs*. The underlying code is displayed in the following code editor box. You'll see that we're using `println!()` to print the *Hello world!* text and this is defined inside the `main()` function.
-            - Secondly, we'll compile the file by running `rustc hello.rs`
-            - Thirdly, we'll run the compiled file using `./hello`
+- First, we'll create a file called *hello.rs*. The underlying code is displayed in the following code editor box. You'll see that we're using `println!()` to print the *Hello world!* text and this is defined inside the `main()` function.
+- Secondly, we'll compile the file by running `rustc hello.rs`
+- Thirdly, we'll run the compiled file using `./hello`
         """)
 
         with open(f'content/{code_dict[code_selection]}') as rust_file:
