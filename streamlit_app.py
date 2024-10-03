a1123cf334a459d92e862e1a5f1c8a6a4fe338bc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-10-02 22:30:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ with col[0]:
             st.session_state.rust_code = rust_file.read()
     
 
-    st.session_state.editor_code = st_ace(st.session_state.rust_code, language='rust', max_lines=8)
+    st.session_state.editor_code = st_ace(st.session_state.rust_code, language='rust', min_lines=8)
 
     
 with col[1]:
