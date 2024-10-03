a8e277f52d986cf96332b1ee0bdfb8229e9fd0af
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-10-02 20:23:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,8 @@ with col[0]:
     with open(f'content/{code_dict[code_selection]}') as rust_file:
         rust_code = rust_file.read()
         
-    if 'previous_code' not in st.session_state:
-        st.session_state.previous_code = rust_code
+    #if 'previous_code' not in st.session_state:
+        #st.session_state.previous_code = rust_code
     
     
     response_dict = code_editor(rust_code, lang='rust', height=12, buttons=btn_settings, key='code_editor')
@@ -72,7 +72,7 @@ with col[0]:
 with col[1]:
     if st.session_state.current_code != '':
         st.subheader('Code Content')
-        st.code(st.session_state.current_code, line_numbers=True)
+        st.code(rust_code, line_numbers=True)
         
         st.subheader('Code Output')
         if 'rust_output' in st.session_state:
