849fe9567d2906446e1f3f8ddc22e9782c3c9ba5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-10-02 21:19:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import subprocess
 import json
-from code_editor import code_editor
 from streamlit_ace import st_ace
 
 st.set_page_config(page_title='Rust in Streamlit', page_icon='🦀', layout='wide')
@@ -22,12 +21,6 @@ if 'rust_code' not in st.session_state:
 
 if 'editor_code' not in st.session_state:
     st.session_state.editor_code = ''
-
-
-#def code_changed():
-    #if 'current_code' in st.session_state and st.session_state.current_code != st.session_state.previous_code:
-        #st.session_state.rust_output = run_rust_code(st.session_state.current_code)
-        #st.session_state.previous_code = st.session_state.current_code
 
 
 st.title('🦀 Rust in Streamlit')
@@ -64,46 +57,15 @@ with col[0]:
         with open(f'content/{code_dict[code_selection]}') as rust_file:
             st.session_state.rust_code = rust_file.read()
     
-    with open('content/btn_settings.json', 'r') as btn_file:
-        btn_settings = json.load(btn_file)
-    
-    #with open('content/hello.rs') as rust_file:
-    #with open(f'content/{code_dict[code_selection]}') as rust_file:
-        #st.session_state.rust_code = rust_file.read()
-        
-    #if 'previous_code' not in st.session_state:
-        #st.session_state.previous_code = rust_code
-    
+
     st.session_state.editor_code = st_ace(st.session_state.rust_code)
-    # st.session_state.editor_code = code_editor(st.session_state.rust_code, lang='rust', height=12, buttons=btn_settings, key='code_editor')
-    #st.session_state.current_code = response_dict['text']
-
-    #code_changed()
-
-    #if st.session_state.current_code != '':
-        #st.warning('Click on the **update** button in the code editor box above.', icon='⚠️')
 
     
 with col[1]:
     if st.button('Run Code'):
+        st.subheader('Code Content')
         st.code(st.session_state.editor_code, line_numbers=True)
+
+        st.subheader('Code Output')
         output = run_rust_code(st.session_state.editor_code)
         st.code(output, line_numbers=True)
-    #if st.session_state.current_code != '':
-        #st.subheader('Code Content')
-        #st.code(rust_code, line_numbers=True)
-        
-        #st.subheader('Code Output')
-        #if 'rust_output' in st.session_state:
-            #st.code(st.session_state.rust_output)
-    #else:
-        #st.warning('Click on the **update** button in the code editor box shown on the left to run the code.', icon='⚠️')
-
-
-st.code(st.session_state.rust_code)
-#st.session_state.current_code
-#st.session_state.previous_code
-
-#st.code(rust_code, line_numbers=True)
-#output = run_rust_code(rust_code)
-#st.code(output, line_numbers=True)
