56c210aa5a2b571d0edb9bbbb40c1a7cac99a4b0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-10-03 19:39:10 -0700

    Update variable.rs

--- content/variable.rs
+++ content/variable.rs
@@ -1,6 +1,8 @@
 fn main() {
     let x = 5u32;
     let y = true;
+    let z = String::from("Hello world");
     println!("The value of x: {:?}", x);
     println!("The value of y: {:?}", y);
+    println!("The value of z: {:?}", z);
 }
